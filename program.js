10a00dc46e4dea731fe7735deec073ef4571e301
Radhika Dangal <rdangal@pgh.yearup.org>
2023-04-25 10:24:46 -0400

    first initial commit

---
+++ program.js
@@ -0,0 +1,13 @@
+// var firstName;
+// firstName = "Radhika";
+// lastName = "Dangal";
+// console.log("Hi! my name is: " + firstName + " " + lastName);
+
+var customerId = 555;
+customer firstName = "Radhika";
+customer lastName = "Dangal";
+console.log(customerId);
+console.log(customer firstName);
+console.log(customer lastName);
+
+
